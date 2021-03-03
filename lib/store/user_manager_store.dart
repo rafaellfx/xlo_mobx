54117d077b83c3e1fe71d38464905605645255c2
Rafael Lima Felix <rafaellfx@gmail.com>
2021-03-02 21:33:25 -0300

    mantendo user logado

--- lib/store/user_manager_store.dart
+++ lib/store/user_manager_store.dart
@@ -1,5 +1,8 @@
+import 'dart:ffi';
+
 import 'package:mobx/mobx.dart';
 import 'package:xlo_mobx/models/user.dart';
+import 'package:xlo_mobx/repositories/user_repository.dart';
 
 part 'user_manager_store.g.dart';
 
@@ -7,6 +10,9 @@ class UserManagerStore = _UserManagerStore with _$UserManagerStore;
 
 abstract class _UserManagerStore with Store {
 
+  _UserManagerStore(){
+    _getCurrentUser();
+  }
 
   @observable
   User user;
@@ -16,6 +22,11 @@ abstract class _UserManagerStore with Store {
 
   @computed
   bool get isLoggedIn => user != null;
+
+  Future<void> _getCurrentUser() async {
+    final user = await UserRepository().currentUser();
+    setUser(user);
+  }
 
 
 
